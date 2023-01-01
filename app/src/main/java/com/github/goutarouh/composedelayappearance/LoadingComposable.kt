ccdaa9bf9b315e9b2ce1d3e692516a86f995c83d
goutarouh <gouta.gorogoro@gmail.com>
2023-01-01 17:46:14 +0900

    ModifierでdelayAppearanceを実装

--- app/src/main/java/com/github/goutarouh/composedelayappearance/LoadingComposable.kt
+++ app/src/main/java/com/github/goutarouh/composedelayappearance/LoadingComposable.kt
@@ -6,27 +6,53 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.graphicsLayer
 import kotlinx.coroutines.delay
 
 @Composable
-fun LoadingScreen(
-    delayMills: Long = 300L,
-    modifier: Modifier = Modifier
-) {
-
-    var isDisplay by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        delay(delayMills)
-        isDisplay = true
-    }
-
-    if (isDisplay) {
-        Box(
-            modifier = modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator()
-        }
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize().delayAppearance(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
     }
 }
+
+fun Modifier.delayAppearance(
+    delayMills: Long = 300L
+) = composed {
+    var isShow by remember { mutableStateOf(false) }
+    LaunchedEffect(Unit) {
+        delay(delayMills)
+        isShow = true
+    }
+    this.graphicsLayer {
+        alpha = if (isShow) 1f else 0f
+    }
+}
+
+
+//@Composable
+//fun LoadingScreen2(
+//    delayMills: Long = 300L,
+//    modifier: Modifier = Modifier
+//) {
+//
+//    var isDisplay by remember { mutableStateOf(false) }
+//
+//    LaunchedEffect(Unit) {
+//        delay(delayMills)
+//        isDisplay = true
+//    }
+//
+//    if (isDisplay) {
+//        Box(
+//            modifier = modifier.fillMaxSize(),
+//            contentAlignment = Alignment.Center
+//        ) {
+//            CircularProgressIndicator()
+//        }
+//    }
+//}
